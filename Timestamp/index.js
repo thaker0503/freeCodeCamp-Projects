4494ad652c60212be0034a2e89cd1da5c58ca2d6
Yatharth Thaker <52533890+thaker0503@users.noreply.github.com>
2022-12-04 04:19:07 +0530

    Update index.js

--- Timestamp/index.js
+++ Timestamp/index.js
@@ -24,7 +24,7 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/timestamp/:date?", (req, res) => {
+app.get("/api/:date?", (req, res) => {
   let date = req.params.date;
   if (!date) {
     date = new Date();
